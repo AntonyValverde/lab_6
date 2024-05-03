c32b373df19526c9369efb168c64218f1baf3d97
Jordan Laguna <jordanlaguna10@gmail.com>
2024-05-03 11:07:13 -0600

    add proyect in the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,13 @@ import 'package:smart_admin_dashboard/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
