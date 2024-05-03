4c13a35e79d486e20b700a93c7bf15662429b810
Jordan Laguna <jordanlaguna10@gmail.com>
2024-05-03 16:24:49 -0600

    chore: Add Firebase dependencies and configuration

---
+++ web/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBU-om8QWRruysHRLKoqZaBP4bEh2KhruM",
+  authDomain: "lab6-e3b70.firebaseapp.com",
+  databaseURL: "https://lab6-e3b70-default-rtdb.firebaseio.com",
+  projectId: "lab6-e3b70",
+  storageBucket: "lab6-e3b70.appspot.com",
+  messagingSenderId: "109392965339",
+  appId: "1:109392965339:web:1a687d9e85bafd26c926fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
